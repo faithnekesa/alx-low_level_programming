22c52c5b1a377c950a9787889cf747ba41ae8a37
faithnekesa <faynekesa@gmail.com>
2023-04-20 07:54:59 -0700

    Add puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 int main(void)
 {
+/**
+ * main is the Entry point of the program
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ */
 	char Mystr1[55] = "\"Programming is like building a multilingual puzzle";
+
 	puts(Mystr1);
 	return (0);
 }
