291c67d732e4cee81a5a9f6c89f63d00d6b30685
faithnekesa <faynekesa@gmail.com>
2023-04-20 08:07:36 -0700

    Add puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  * 
- * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Description: 'The program prints \"Programming is like building a multilingual puzzle'
  * @parameter: describe the parameter
+ * 
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
