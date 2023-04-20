71bdc038208e3bece42ec4e362618073890d1833
faithnekesa <faynekesa@gmail.com>
2023-04-20 08:00:49 -0700

    Add puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-
 /**
- * main is the Entry point of the program
+ * main - Entry point
+ * 
  * Description: prints "\"Programming is like building a multilingual puzzle"
- * Return: 0
+ * @parameter: describe the parameter
+ * 
+ * Return: Always 0 (Success)
  */
 
 int main(void)
