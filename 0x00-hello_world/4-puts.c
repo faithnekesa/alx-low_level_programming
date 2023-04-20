0c1d239975a2d25f07002df33683b11b5da56099
faithnekesa <faynekesa@gmail.com>
2023-04-20 07:52:03 -0700

    Add puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-	char Mystr1[50] = "Programming is like building a multilingual puzzle";
+	char Mystr1[55] = "\"Programming is like building a multilingual puzzle";
 	puts(Mystr1);
 	return (0);
 }
