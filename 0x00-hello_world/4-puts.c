842ec364520d9108b9823078093496713f74768a
faithnekesa <faynekesa@gmail.com>
2023-04-20 14:16:00 -0700

    Adding printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- * 
+ * main - Entry point 
  * Description: 'The program prints \"Programming is like building a multilingual puzzle'
- * @parameter: describe the parameter
- * 
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
+	inputStr[end+1] = '\0';
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
