8d28d31f81c210f56ec4bc83f61c425b52256450
faithnekesa <faynekesa@gmail.com>
2023-04-25 08:27:58 -0700

    Adding sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - returns 1 if num is +, - if num < 0 and 0 otherwise
+ * @num: integer to be tested
+ * Return: 1, -1 or 0
+ */
+int print_sign(int num)
+{
+	if (num > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (num == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
