829ab31d99f6daf47bb32fc2093aa0da738a16dd
faithnekesa <faynekesa@gmail.com>
2023-04-25 15:14:30 -0700

    Adding fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,19 @@
+/**
+ * main - prints the sum of the even-valued terms under 4000000
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long int i = 1, j = 2, sum = j, tmp;
+
+	while (j < 4000000)
+	{
+		tmp = i + j;
+		i = j;
+		j = tmp;
+		sum += (j % 2 == 0) ? f2 : 0;
+	}
+	printf("%li\n", sum);
+	return (0);
+}
