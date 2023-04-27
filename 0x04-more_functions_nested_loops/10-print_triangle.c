186f0a1051cad2ae1bb5eee471e5ee67d5edcdc3
faithnekesa <faynekesa@gmail.com>
2023-04-27 09:59:38 -0700

    Adding triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,25 +6,24 @@
  */
 void print_triangle(int size)
 {
+	int row, hash, space;
+
 	if (size <= 0)
 	{
 		_putchar('\n');
-	} else
+	}
+	else
 	{
-		int i, j;
-
-		for (i = 1; i <= size; i++)
+		for (row = 1; row <= size; row++)
 		{
-			for (j = i; j < size; j++)
+			for (space = size - row; space >= 1; space--)
 			{
 				_putchar(' ');
 			}
-
-			for (j = 1; j <= i; j++)
+			for (hash = 1; hash <= row; hash++)
 			{
 				_putchar('#');
 			}
-
 			_putchar('\n');
 		}
 	}
