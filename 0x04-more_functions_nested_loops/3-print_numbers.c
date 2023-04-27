5a70d65c57f2d3da02110be20e478ce0276de300
faithnekesa <faynekesa@gmail.com>
2023-04-27 09:47:51 -0700

    Adding print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,17 @@
 #include "main.h"
 
 /**
-*print_numbers - print numbers between o and 9
-*Return: void
-*/
-ivoid print_numbers(void)
+ * print_numbers - prints the numbers, from 0 to 9,
+ * followed by a new line
+ */
+void print_numbers(void)
 {
 	int i;
 
 	for (i = 0; i < 10; i++)
-		_putchar(i);
+	{
+		_putchar(i + '0');
+	}
+
 	_putchar('\n');
+}
