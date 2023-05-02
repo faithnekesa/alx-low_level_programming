99e8c9fc9566a8605c6a5b5cf82f0206bd92fd18
faithnekesa <faynekesa@gmail.com>
2023-05-02 09:15:45 -0700

    adding print reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,10 +7,12 @@
  */
 void print_rev(char *s)
 {
-	int i, j, len;	
+	int i, j, len;
 
-	for (i=0; s[i] != '\0'; i++)
-	
+	i = 0;
+
+	while (s[i] != '\0')
+	i++
 	len = i;
 
 	for (j = len - 1; j >= 0; j--)
