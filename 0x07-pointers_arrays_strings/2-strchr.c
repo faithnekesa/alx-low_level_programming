aabe663fc6e8fe5c9b919bda193366c96c49d881
faithnekesa <faynekesa@gmail.com>
2023-05-08 07:52:30 -0700

    Adding string char search

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -20,20 +20,3 @@ char *_strchr(char *s, char c)
 
 	return ('\0');
 }
-
-
-char *_strchr(char *s, char c)
-{
-	int i;
-
-	for (i = 0; *(s + i) != '\0'; i++)
-	{
-		if (*(s + i) == c)
-			return (s + i);
-
-		if (*(s + i) == c)
-			return (s + i);
-	}
-
-	return ('\0');
-}
