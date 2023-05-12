84f101f3596ca6746c27c28771707a4da80c2404
faithnekesa <faynekesa@gmail.com>
2023-05-12 13:46:07 -0700

    Adding static library

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,1 +1,15 @@
 #include "main.h"
+
+/**
+ * _puts - Function that prints a string
+ * @str: pointer to the string to print
+ */
+void _puts(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
