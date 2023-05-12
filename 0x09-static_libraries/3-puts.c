6b54e6bc715fd72b3e20c0da9cac48f38690a03a
faithnekesa <faynekesa@gmail.com>
2023-05-12 13:34:00 -0700

    Adding puts

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,17 +1,1 @@
 #include "main.h"
-
-/**
- * _puts - prints a string followed by a new line to stdout
- * @str: string to be printed
- *
- *Return: void
- */
-void _puts(char *str)
-{
-	while (*str)
-	{
-		_putchar(*str);
-		str++;
-	}
-	_putchar('\n');
-}
