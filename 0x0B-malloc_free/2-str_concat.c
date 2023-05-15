50c95131e296c3796ccf89bade222f905eeaf590
faithnekesa <faynekesa@gmail.com>
2023-05-15 07:57:22 -0700

    Adding string concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -5,45 +5,49 @@
  * str_concat - function that concatenates two strings
  * @s1: String 1 to be concatenated
  * @s2: String 2 to be concatenated
- * Return: pointer pointing to newly allocated space in memory with s1 and s2
+ * Return: pointer to newly allocated memory with s1 and s2
  * and NULL on failure
  */
-
 char *str_concat(char *s1, char *s2)
 {
-	int counter1, counter2, result_len, len1, len2;
 	char *concat_result;
+	unsigned int counter1, counter2, len1, len2 = 0;
 
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
-
 	if (s1 != NULL)
 	{
-		counter1 = 0;
 		while (s1[counter1++] != '\0')
 			len1++;
 	}
-
+	concat_result = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (concat_result == NULL)
+		return (NULL);
 	if (s2 != NULL)
 	{
 		counter1 = 0;
 		while (s2[counter1++] != '\0')
 			len2++;
 	}
-
-	result_len = len1 + len2;
-	concat_result = malloc(sizeof(char) * result_len);
-	if (concat_result == NULL)
-		return (NULL);
-
-	for (counter1 = 0; counter1 < len1; counter1++)
-		concat_result[counter1] = s1[counter1];
-	for (counter2 = 0; counter2 < len2; counter2++, counter1++)
-		concat_result[counter1] = s2[counter2];
-	concat_result[result_len] = '\0';
-
+	counter1, counter2 = 0;
+	if (s1)
+	{
+		while (counter1 < len1)
+		{
+			concat_result[counter1] = s1[counter1];
+			counter1++;
+		}
+	}
+	if (s2)
+	{
+		while (counter1 < (len1 + len2))
+		{
+			concat_result[counter1] = s2[counter2];
+			counter1++, counter2++;
+		}
+	}
+	concat_result[counter1] = '\0';
 	return (concat_result);
 }
