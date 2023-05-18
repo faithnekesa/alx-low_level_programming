392fcd71c948285cf85932023118e67d71ae2931
faithnekesa <faynekesa@gmail.com>
2023-05-18 14:07:38 -0700

    Correcting string nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -5,34 +5,41 @@
  * string_nconcat- function that concatenates all arguments of the program
  * @s1: pointer to the first string
  * @s2: pointer to the second string
- * @n: size of the pointer
+ * @n: pointer size
  * Return: pointer to a new string, or NULL if it fails
  * if NULL is passed, treat it as an empty string
 */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *new_str;
-	unsigned int s1_len, s2_len, counter, concat_string;
-	{
-		if (s1 == NULL || s2 == NULL)
-		{
-			for (s1_len = 0; s1[s1_len] != '\0'; s1_len++)
-				;
-			for (s2_len = 0; s2[s2_len] != '\0'; s2_len++)
-				;
-		if (n > s2_len)
-			n = s2_len;
-		concat_string = s1_len + n;
-		new_str = malloc(concat_string + 1);
-		if (new_str == NULL)
-			return (NULL);
-		for (counter = 0; counter < concat_string; counter++)
-		{
-			if (counter < s1_len)
-				new_str[counter] = s1[counter];
-			else
-				new_str[counter] = s2[counter - s1_len];
-			new_str[counter] = '\0';
-			return (new_str);
-		}
+unsigned int s1_len, s2_len, counter;
+char *new_str;
+
+if (s1 == NULL)
+	s1 = "";
+if (s2 == NULL)
+	s2 = "";
+
+for (s1_len = 0; s1[s1_len] != '\0'; s1_len++)
+	;
+
+for (s2_len = 0; s2[s2_len] != '\0'; s2_len++)
+	;
+
+	if (n >= s2_len)
+		n = s2_len;
+
+new_str = malloc(sizeof(char) * n + s1_len + 1);
+if (new_str == NULL)
+	return (NULL);
+
+for (counter = 0; counter < s1_len; counter++)
+	new_str[counter] = s1[counter];
+
+for (counter = 0; counter < n; counter++)
+	new_str[counter + s1_len] = s2[counter];
+
+	new_str[counter + s1_len] = '\0';
+
+	return (new_str);
+}
