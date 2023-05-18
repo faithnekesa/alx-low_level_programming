bf6de05ba54fb9213312188fb538dc81eafa18a1
faithnekesa <faynekesa@gmail.com>
2023-05-18 14:23:00 -0700

    Correcting string nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,25 +14,34 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 unsigned int s1_len, s2_len, counter;
 char *new_str;
+s1_len = 0;
+s2_len = 0;
 
 if (s1 == NULL)
 	s1 = "";
 if (s2 == NULL)
 	s2 = "";
 
-for (s1_len = 0; s1[s1_len] != '\0'; s1_len++)
-	;
 
-for (s2_len = 0; s2[s2_len] != '\0'; s2_len++)
-	;
+if (s1 != NULL)
+{
+counter = 0;
+while (s1[counter++] != '\0')
+	s1_len++;
+}
 
-	if (n >= s2_len)
-		n = s2_len;
+if (s2 != NULL)
+{
+counter = 0;
+while (s2[counter++] != '\0')
+	s2_len++;
+}
 
+if (n >= s2_len)
+	n = s2_len;
 new_str = malloc(sizeof(char) * n + s1_len + 1);
 if (new_str == NULL)
 	return (NULL);
-
 for (counter = 0; counter < s1_len; counter++)
 	new_str[counter] = s1[counter];
 
