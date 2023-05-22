2c40ee0404eb53f228d117cd590620d71c3e98f5
faithnekesa <faynekesa@gmail.com>
2023-05-22 02:29:20 -0700

    Correcting betty

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,16 +6,14 @@
  * @d: pointer to the struct dog.
  * Return: print nil If an element of d is NULL
  * Return nothing if d is NULL
- */
+*/
 void print_dog(struct dog *d)
 {
-        if (d == NULL)
-                return;
-
-        if (d->name == NULL)
-                d->name = "(nil)";
-        if (d->owner == NULL)
-                d->owner = "(nil)";
-
-        printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+if (d == NULL)
+	return;
+if (d->name == NULL)
+	d->name = "(nil)";
+if (d->owner == NULL)
+	d->owner = "(nil)";
+printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
