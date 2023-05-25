8716957c8c8a177093fb1f2244fad2dcf86a2081
faithnekesa <faynekesa@gmail.com>
2023-05-25 14:46:28 -0700

    Correcting requirement for NULL

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,7 +9,7 @@ void print_all(const char * const format, ...)
 {
 va_list list;
 char *ptr;
-unsigned int a = 0, b = 0;
+unsigned int a = 0, b = 1;
 
 va_start(list, format);
 while (format && format[a] != '\0')
@@ -18,25 +18,23 @@ while (format && format[a] != '\0')
 	{ case 'c':
 	switch (b)
 	{ case 1: printf(", "); }
-	b = 1;
 	printf("%c", va_arg(list, int));
 	break;
 	case 'i':
 	switch (b)
 	{ case 1: printf(", "); }
-	b = 1;
 	printf("%i", va_arg(list, int));
 	break;
 	case 'f':
 	switch (b)
 	{ case 1: printf(", "); }
-	b = 1;
 	printf("%f", va_arg(list, double));
 	break;
 	case's':
 	switch (b)
 	{ case 1: printf(", "); }
-	b = 1;
+	if (!ptr)
+		ptr = "(nil)";
 	ptr = va_arg(list, char*);
 	if (ptr)
 	{ printf("%s", ptr);
