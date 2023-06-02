f4a2a75397ab1927b20f75991d92d5632b9f9c9b
faithnekesa <faynekesa@gmail.com>
2023-06-02 06:43:24 -0700

    Correcting betty

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,15 +12,14 @@ size_t print_list(const list_t *h)
 size_t node_count = 0;
 list_t *node = h;
 
- while (node)
- {
- if (!node->str)
- printf("[0] (nil)\n");
- else
- printf("[%zu] %s\n", node->len, node->str);
- node = node->next;
- node_count++;
- }
-
- return (node_count);
+while (node)
+{
+	if (!node->str)
+		printf("[0] (nil)\n");
+	else
+		printf("[%zu] %s\n", node->len, node->str);
+	node = node->next;
+	node_count++;
+}
+return (node_count);
 }
