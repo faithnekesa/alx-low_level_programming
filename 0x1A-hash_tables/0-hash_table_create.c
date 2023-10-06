6515df7ea85e9351889726e070bb261cc8a3273d
faithnekesa <faynekesa@gmail.com>
2023-10-06 01:30:28 -0700

    Correcting betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,8 +3,7 @@
 /**
  * hash_table_create -Function that creates a hash table
  * @size:The array size
- * Return:A pointer to the newly created hash table
-		  Otherwise -NULL
+ * Return:A pointer to the newly created hash table Otherwise -NULL
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
